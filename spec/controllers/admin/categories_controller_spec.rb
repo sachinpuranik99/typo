897c53d059a899128d7ad4db45723451937ba765
EC2 Default User <ec2-user@ip-172-31-9-130.us-east-2.compute.internal>
2018-10-12 04:21:16 +0000

    Final commit

--- spec/controllers/admin/categories_controller_spec.rb
+++ spec/controllers/admin/categories_controller_spec.rb
@@ -10,11 +10,36 @@ describe Admin::CategoriesController do
     henri = Factory(:user, :login => 'henri', :profile => Factory(:profile_admin, :label => Profile::ADMIN))
     request.session = { :user => henri.id }
   end
+  
+  
 
   it "test_index" do
     get :index
     assert_response :redirect, :action => 'index'
   end
+
+  it "test_create" do
+    cat = Factory(:category)
+    Category.should_receive(:find).with(:all).and_return([])
+    Category.should_receive(:new).and_return(cat)
+    cat.should_receive(:save!).and_return(true)
+    post :edit, 'category' => { :name => "test_cat",:keywords=> "test_keywords",:permalink => "link", :description => "this is a description"}
+    assert_response :redirect
+    assert_redirected_to :action => 'new'
+  end
+  
+  describe "test_new" do
+    before(:each) do
+      get :new
+    end
+
+    it 'should render template new' do
+      assert_template 'new'
+      assert_tag :tag => "table",
+        :attributes => { :id => "category_container" }
+    end
+  end
+
 
   describe "test_edit" do
     before(:each) do
